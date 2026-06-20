985503b26a583d6ca985381f7dde63cf4ff40f3e
whiteBear08 <whiteslurpybear08@gmail.com>
2026-06-20 16:47:09 +0000

    container config done

---
+++ 01_basics/03_conversionAndOperations.js
@@ -0,0 +1,70 @@
+let score = "33"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)   // it change the type of 
+// score from string to number
+// console.log(typeof valueInNumber) 
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+// null => 0
+// undefined => NaN 
+
+let idLoggedIn = "Bear"
+
+let booleanIsLoggedIn = Boolean(idLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// "" => false
+// "Bear" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+//  *********************  Operations in JavaScript  ************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 ** 3)
+// console.log(2 / 3)
+// console.log(2 % 3)
+
+let str1 = "Hello"
+let str2 = " Bear"
+
+let str3 = str1 + str2
+// console.log(str3)
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log(true)
+// console.log(+true) // 1
+// console.log(false)
+// console.log(+false) // 0
+// console.log(+"")
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
+
+
+
